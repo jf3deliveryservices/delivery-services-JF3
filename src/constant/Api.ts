46bef2a52bd4bd78e02509ac73f79b9214937a73
jf3deliveryservices <jf3deliveryservices@gmail.com>
2025-10-24 13:07:26 -0400

    avances

--- src/constant/Api.ts
+++ src/constant/Api.ts
@@ -1,6 +1,6 @@
 import axios, { type AxiosResponse } from "axios";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "https://jf3-service-production.up.railway.app";
 
 //----------------- TYPES -----------------//
 
